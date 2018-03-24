5a2cf594073f1debce8ca7616d932e8e8cecc573
Vinod Prakash <vinod@vakilsearch.com>
2018-03-24 12:50:56 +0530

    Redirection in href

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-	default from: 'notifications@example.com'
- 
+	default from: 'winkhizevents@gmail.com'
+
   def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    @url  = 'http://winkhiz.com'
+    mail(to: @user.email, subject: 'Thanks you for contacting us !')
   end
   def website_enquiry(user)
     @user = user
